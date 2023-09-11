77963c113b3e2e4a5748fea6874fd1aec3f6ae9e
Michaaaaal <mvotypka7@gmail.com>
2023-09-11 14:57:47 +0200

    added info

---
+++ prg1.cs
@@ -0,0 +1,20 @@
+using system;
+
+interface Irectangle {
+void render();
+
+
+}
+class rectangle{
+private int v;
+private int w;
+
+public void render(){
+Console.WriteLine("renderuju");
+}
+}
+private void changeshape(){
+ Console.WriteLine("menim");
+}   
+//nestihl sem vse opsat :(
+
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
 tohle je uvod do prg neco
+
 git add .
+
 git commit -m "added info"
+
 git push
+
