d1aed3bd1addecf3f2777968d81a48817ec1ea69
Michaaaaal <mvotypka7@gmail.com>
2023-09-17 22:06:12 +0200

    Ukol

---
+++ homework_01/CalculatorContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_01
+{
+    internal class CalculatorContext
+    {
+        private readonly IOperation _opStrategy;//Asociace s rozhranim IOperation
+
+        public CalculatorContext(IOperation opStrategy) 
+        {
+            this._opStrategy = opStrategy;
+        }
+
+        public int ExecuteStrategy(int a, int b)
+        {
+            return _opStrategy.Execute(a, b);
+        }
+    }
+}
---
+++ homework_01/OpSub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_01
+{
+    internal class OpSub : IOperation // implementace rozhrani IOperation
+    {
+        public int Execute(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
